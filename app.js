d9eebd70c6ba1503a2997b58afa80cc8e58c8a4b
hyuk-dev <hyuk.development@gmail.com>
2025-05-14 14:28:57 +0900

    feat: 데이터베이스 연결 비활성화

--- app.js
+++ app.js
@@ -4,37 +4,37 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const { Sequelize, DataTypes } = require("sequelize");
 
-dotenv.config();
-const config = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
-  dialect: process.env.DB_DIALECT,
-};
+// dotenv.config();
+// const config = {
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT, 10),
+//   dialect: process.env.DB_DIALECT,
+// };
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+// const sequelize = new Sequelize(
+//   config.database,
+//   config.username,
+//   config.password,
+//   config
+// );
 
-const User = sequelize.define("User", {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  age: {
-    type: DataTypes.INTEGER,
-  },
-});
+// const User = sequelize.define("User", {
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   age: {
+//     type: DataTypes.INTEGER,
+//   },
+// });
 
-sequelize
-  .authenticate()
-  .then(() => console.log("DB 연결 성공"))
-  .catch((err) => console.error("DB 연결 실패", err));
+// sequelize
+//   .authenticate()
+//   .then(() => console.log("DB 연결 성공"))
+//   .catch((err) => console.error("DB 연결 실패", err));
 
 const app = express();
 
--- tests/app.test.js
+++ tests/app.test.js
@@ -3,8 +3,8 @@ const app = require("../app");
 
 describe("GET /", () => {
   it("기본 경로(/)는 200 상태코드와 함께 환영 메시지를 응답해야 합니다.", async () => {
-    const response = await request(app).get('/');
+    const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello World from Express!');
+    expect(response.text).toBe("Hello World from Express!");
   });
 });
